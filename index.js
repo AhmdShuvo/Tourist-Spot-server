2f4068569010cd7a5f753ee9d59c5d03a8f48cbf
AhmdShuvo <tanzirshovo2@gmail.com>
2023-01-25 22:14:59 +0600

    blogs management

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-const express = require("express");
-const cors = require("cors")
-const app = (express())
+const { MongoClient, Admin } = require("mongodb");
+const Objectid = require('mongodb').ObjectId;
+const express = require('express');
+const cors = require('cors');
 require("dotenv").config();
-const Objectid=require('mongodb').ObjectId;
-const { MongoClient} = require("mongodb");
-const { query } = require("express");
 
+// USED FOR READING FILE IN EXPRESS//
+const fileUpload = require('express-fileupload')
 
+const app = (express())
 
-// try port 9000//
 const port = process.env.PORT || 9000;
 
-    app.use(cors());
 
-    app.use(express.json());
-
-
-
+app.use(express.json())
+app.use(cors())
+app.use(fileUpload())
     app.get('/', async (req, res) => {
 
         res.send("server Running")
     })
 
+    
     const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0qtlc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     
@@ -34,6 +33,7 @@ const port = process.env.PORT || 9000;
             const database = client.db('TouristSpot');
             const placesCollection = database.collection('places');
             const ordersCollection = database.collection('orders');
+            const BlogsCollection = database.collection('Blogs')
     
                     //    get data from data base to url ///
             app.get('/places',async(req,res)=>{
@@ -78,7 +78,40 @@ const port = process.env.PORT || 9000;
                 res.json(users)
             }) 
 
+            // POST BLOGS //
 
+            app.post("/blogs",async(req,res)=>{
+                const data=req.body;
+                const result=await BlogsCollection.insertOne(data);
+                res.send(result)
+
+            })
+            app.get("/blog/:id", async (req, res) => {
+
+                const id = req.params.id;
+                const query = { _id: Objectid(id) }
+              
+                const result = await BlogsCollection.findOne(query);
+                res.json(result)
+              });
+              
+              app.delete('/blog/:id', async (req, res) => {
+              
+                const id = req.params.id;
+                const query = { _id: Objectid(id) }
+              
+                const result = await BlogsCollection.deleteOne(query);
+              
+                res.send(result)
+              });
+              
+              
+                app.get("/blogs",async(req,res)=>{
+                 const cursor= BlogsCollection.find({});
+                 const result= await cursor.toArray();
+                 res.json(result)
+                
+                });
             // CHANGE STATUS ///
     
             app.put("/order/:id",async(req,res)=>{
